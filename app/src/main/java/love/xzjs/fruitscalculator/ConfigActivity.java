0c7a62d163498aac121e96172d3a4afdac5d9492
xzjs <2310976219@qq.com>
2017-12-11 23:58:46 +0800

    添加configActivity，itemActivity

--- app/src/main/java/love/xzjs/fruitscalculator/ConfigActivity.java
+++ app/src/main/java/love/xzjs/fruitscalculator/ConfigActivity.java
@@ -6,19 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-    ImageButton configButton;
+public class ConfigActivity extends AppCompatActivity {
 
+    ImageButton addBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        configButton = findViewById(R.id.config);
-        configButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_config);
+        addBtn=findViewById(R.id.addItem);
+        addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, LoginActivity.class);
+                intent.setClass(ConfigActivity.this, ItemActivity.class);
                 startActivity(intent);
             }
         });
