99b09d8d3efe3bff08e93906e0046c75f0f17e96
xzjs <2310976219@qq.com>
2017-12-12 08:56:20 +0800

    添加configActivity，itemActivity

--- app/src/main/java/love/xzjs/fruitscalculator/ItemActivity.java
+++ app/src/main/java/love/xzjs/fruitscalculator/ItemActivity.java
@@ -1,13 +1,77 @@
 package love.xzjs.fruitscalculator;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.jar.Manifest;
 
 public class ItemActivity extends AppCompatActivity {
+
+    Button takePhotoBtn, choosePhotoBtn;
+    private static final int CROP_PHOTO = 2;
+    private static final int REQUEST_CODE_PICK_IMAGE = 3;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 6;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE2 = 7;
+    private File output;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+
+        takePhotoBtn = findViewById(R.id.take_photo);
+
+    }
+
+    public void takePhoto(View view) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_CALL_PHONE2);
+        } else {
+            File file = new File(Environment.getExternalStorageDirectory(), "takePhoto");
+            if (!file.exists()) {
+                file.mkdir();
+            }
+            output = new File(file, System.currentTimeMillis() + ".jpg");
+            try {
+                if (output.exists()) {
+                    output.delete();
+                }
+                output.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            imageUri = Uri.fromFile(output);
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, CROP_PHOTO);
+        }
+    }
+
+    public void onActivityResult(int req,int res,Intent data){
+        switch (req){
+            case CROP_PHOTO:
+                if(res==RESULT_OK){
+                    try{
+                        Bitmap bit= BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }
     }
 }
